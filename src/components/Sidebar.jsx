7b5243636665d4d5278108b1546a2364bb96fe69
Bishal Jana <bishaljana2026@gmail.com>
2024-05-02 22:20:52 +0530

    search bar setup

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import Otherusers from "./Otherusers";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setOtherUser } from "../redux/userSlice";
 
 function Sidebar() {
+  const [search, setSearch] = useState("");
+  const { otherUsers } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const searchHandeler = (e) => {
+    e.preventDefault();
+    const searchUser = otherUsers?.find((user) =>
+      user.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    if (searchUser) {
+      dispatch(setOtherUser([searchUser]));
+    } else {
+      toast.error("User not found");
+    }
+  };
+
   const naviagate = useNavigate();
   const logoutHandeler = async () => {
     try {
@@ -21,8 +38,10 @@ function Sidebar() {
   return (
     <div className=" border-r border-zinc-400 p-4 flex flex-col">
       {" "}
-      <form action="" className="flex items-center">
+      <form onSubmit={searchHandeler} action="" className="flex items-center">
         <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           type="text"
           className="input input-bordered rounded-md"
           placeholder="Search..."
